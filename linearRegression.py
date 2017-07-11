ddac0b2e9a4fa8cfbb5b0f14d018adc7936cb401
Jorfun <jorfunliu@qq.com>
2017-07-11 09:49:16 +0800

    clean up

--- linearRegression.py
+++ linearRegression.py
@@ -1,3 +1,6 @@
+#身高，体重，肺活量， 50 米跑，立定跳远，坐位体前屈 -> 长跑成绩
+#只针对女同学
+
 # 导入
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,7 +32,7 @@ def loadData():
         # 方便之后矩阵运算
         vec = [1.0]
         
-        # 把输出和输入存在不同的列表中
+        # 把每行数据的输出和输入存在不同的列表中
         for cell in row:
 
             if(cell.column == 7): 
@@ -58,30 +61,47 @@ def loadData():
     
     
     
+# 对数据进行特征归一化处理
+def normalize(x):
+    xMin = x.min(axis=0)
+    xMax = x.max(axis=0)
+    range =  xMax - xMin
+    range[0] = 1
+    xMin[0] = 0
+    x_norm = (x - xMin) / range
+    
+    return x_norm
+    
+    
+    
     
 # 梯度下降
 def gradDescent(dataMatIn, targetMatIn):
-
+    
+    # 将python的list转换成numpy的ndarray，同时将输入的特征向量规范化
     x = np.array(dataMatIn)
-    xTrans = x.transpose()
+    x_norm = normalize(x)
+    print(x_norm)
+    xTrans = x_norm.transpose()
     y = np.array(targetMatIn)
 
-    alpha = 2.27e-7
-    numIterations = 300000
-    m,n = x.shape
+    alpha = 0.9
+    numIterations = 3000
+    m,n = x_norm.shape
     theta = np.ones(n)
     
     # 用于后面画图表示梯度下降的过程
     JTheta = []
     
+     # 每次循环更新theta时，都使用所有的训练数据
     for i in range(0, numIterations):
-        hypothesis = np.dot(x, theta)
+        hypothesis = np.dot(x_norm, theta)
         # print("hypothesis")
         # print(hypothesis)
         # print("y")
         # print(y)
         loss = hypothesis - y
-        # 平均每个输入相对于当前theta的代价
+        # 计算J(theta) - 平均每个输入相对于当前theta的代价
         cost = np.sum(loss ** 2) / (2 * m)
         JTheta.append(cost)
         print("Iteration %d | Cost: %f" % (i, cost))
@@ -98,23 +118,26 @@ def gradDescent(dataMatIn, targetMatIn):
     
 # 将模型用于测试数据集，观察每个输入的预测输出与真实值得差别
 def testModel(theta, testMat, testTargetMat):
+    
+    # 将python的list转换成numpy的ndarray，同时将输入的特征向量规范化
     x = np.array(testMat)
+    x_norm = normalize(x)
     y = np.array(testTargetMat)
 
-    predict = np.dot(x, theta)
+    predict = np.dot(x_norm, theta)
     loss = predict - y
 
     return loss
-    
- 
 
- 
+
+
+
 #训练和测试模型，绘图
 a1,a2,a3,a4 = loadData()
 theta, JTheta = gradDescent(a1,a2)
 print(theta)
 costValues = testModel(theta,a3,a4)
-print(costValues)
+# print(costValues)
 
 
 # 画出梯度下降过程中代价函数值随迭代过程的变化
@@ -124,24 +147,25 @@ print(costValues)
 # plt.show()
 
 
-# 画出将模型用于测试数据时，预测值与真实值之间的差别情况
-costValues = testModel(theta,a3,a4)
-print(costValues)
-plt.plot(costValues, 'ro')
-plt.ylabel('differential')
-plt.xlabel('test data index')
-plt.show()
-
-
-# 同时画出上面两个图
-# plt.figure(1)
-# plt.subplot(211)
-# plt.plot(JTheta, linewidth=1.0)
-# plt.ylabel('cost function')
-# plt.xlabel('iteration times')
-
-# plt.subplot(212)
+# 画出将模型用于测试数据时，预测值与真实值之间的偏差情况
+# costValues = testModel(theta,a3,a4)
+# print(costValues)
 # plt.plot(costValues, 'ro')
 # plt.ylabel('differential')
 # plt.xlabel('test input')
 # plt.show()
+
+
+# 同时画出上面两个图
+plt.figure(1)
+plt.subplot(211)
+plt.plot(JTheta, linewidth=1.0)
+plt.ylabel('cost function')
+plt.xlabel('iteration times')
+
+plt.subplot(212)
+plt.plot(costValues, 'ro')
+plt.ylabel('differential')
+plt.xlabel('test input')
+plt.show()
+
--- perceptron.py
+++ perceptron.py
@@ -3,46 +3,65 @@ import matplotlib.pyplot as plt
 from random import choice
 
 
+inputDimension = 2
 
-
+    
 # 从csv文件获取所有数据
 def loadData():
 
+    #获取测试和训练数据
     f = open('perceptron(2dim).csv', 'r')
     
-    testData = []
-    testAnswer = []
-    
-    for line in f:
-        temp = line.strip('\n').split(',')
-        testData.append([1, int(temp[0]), int(temp[1])])
-        testAnswer.append(int(temp[2]))
-        
-    return testData, testAnswer
-
-    
-
-    
-# 从csv文件分别获取男生女生的数据
-def loadDataSeperate():
-
-    f = open('perceptron(2dim).csv', 'r')
-    
+    trainInput = []
+    trainOutput = []
+    testInput = []
+    testOutput = []
     boys = []
     girls = []
     
-    for line in f:
+    for idx,line in enumerate(f):
         temp = line.strip('\n').split(',')
-       
-        if(temp[2] == '1'):
-            boys.append([int(temp[0]), int(temp[1])])
-        else:
-            girls.append([int(temp[0]), int(temp[1])])
+        
+        if(idx <= 2000):
+            trainInput.append([1, int(temp[0]), int(temp[1])])
+            trainOutput.append(int(temp[2]))
+        else:           
+            testInput.append([1, int(temp[0]), int(temp[1])])
+            testOutput.append(int(temp[2]))            
             
-    return boys,girls    
+            if(temp[2] == '1'):
+                girls.append([int(temp[0]), int(temp[1])])
+            else:
+                boys.append([int(temp[0]), int(temp[1])])
+     
+    print("boys number:", len(boys))
+    print("girls number", len(girls), "\n")
+     
+    return trainInput, trainOutput, testInput, testOutput, boys, girls    
     
 
+
+
+# 对数据进行特征归一化处理
+def normalize(x):
+    xMin = x.min(axis=0)
+    xMax = x.max(axis=0)
+    range =  xMax - xMin
+    # print("xMin \n", xMin)
+    # print("xMax \n", xMax)
+    # print("range \n", range, "\n\n")
+    m,n = x.shape
     
+    if(n == inputDimension+1):
+        range[0] = 1    #输入数据第一维分量为1时的特殊处理
+        xMin[0] = 0    #输入数据第一维分量为1时的特殊处理
+    
+    x_norm = (x - xMin) / range
+    
+    return x_norm
+
+
+
     
 # 用于感知器的sign函数
 def sign(x):
@@ -53,12 +72,14 @@ def sign(x):
     
 
     
-    
-def pocketAlgorithm(data, answer):
+#  通过口袋算法来学习感知器的权重    
+def pocketAlgorithm(trainInput, trainOutput):
+    trainInput = np.array(trainInput)
+    trainInputNormed = normalize(trainInput)
     tempWeight = np.array([-1,1,1]).transpose()
     bestWeight = tempWeight
     leastErrorMade = 1e10
-    iteration = 1000
+    iteration = 500
     alpha = 0.01
     
     
@@ -69,13 +90,12 @@ def pocketAlgorithm(data, answer):
         errorList = []
         
         # 遍历所有数据得到分类存在问题的所有点
-        for idx, val in enumerate(data):
+        for idx, val in enumerate(trainInputNormed):
             # print(idx, val)
-            # print(val)
-            # print(currentBestWeight)
+            # print(bestWeight)
             predictValue = sign(val.dot(tempWeight))
             
-            if(predictValue != answer[idx]):
+            if(predictValue != trainOutput[idx]):
                 errorList.append(idx)
          
         # 保存当前感知器错误分类的点个数,并判断是否要更新最优感知器
@@ -84,66 +104,73 @@ def pocketAlgorithm(data, answer):
             bestWeight = tempWeight
             leastErrorMade = errorMade
         
-        # print(errorMade)
+        #print("errorMade \n", errorMade, "\n\n")
         
         # 从所有错误的点中随机选一个用来纠正当前的感知器
         randomErrorIndex= choice(errorList)
         
         # 针对随机选的点对感知器进行修正
-        tempWeight = tempWeight + alpha * answer[randomErrorIndex] * data[randomErrorIndex]
-        
+        tempWeight = tempWeight + alpha * trainInputNormed[randomErrorIndex] * trainOutput[randomErrorIndex]
+        #print("tempWeight \n", tempWeight, "\n\n")
+          
         # 确保相应的迭代次数过后退出循环
         iteration -= 1
 
+        
+    print("leastErrorMade:", leastErrorMade, "\n")
+    print("bestWeight:", bestWeight, "\n")
+        
     return bestWeight
 
 
 
+#  测试当前得到感知器在测试数据集上的效果
+def testPerceptron(perceptron, testInput, testOutput):
+    testInput = np.array(testInput)
+    testInputNormed = normalize(testInput)
+    errorCount = 0  #预测错误次数
+    totalTest = 0   #总的预测次数
     
-# 观察男女生在肺活量和立定跳的两个维度上是否线性可分
-# boys,girls = loadDataSeperate()
+    for idx, val in enumerate(testInputNormed):
+        predictValue = sign(val.dot(perceptron))
+        
+        if(predictValue != testOutput[idx]):
+            errorCount += 1     
+        
+        totalTest += 1
+        
+    print("Total test:", totalTest, "  Error:", errorCount, "  Error rate:", errorCount/totalTest)
+    
+    
 
-# boys = np.array(boys)
-# girls = np.array(girls)
+    
+# 画图
+def drawGraph(perceptron, boys, girls):
+    boys = np.array(boys)
+    girls = np.array(girls)
+    boys_normed = normalize(boys)
+    girls_normed = normalize(girls)
+    
+    
+    plt.plot(boys[ : ,0], boys[ : ,1], 'ro')
+    plt.plot(girls[ : ,0], girls[ : ,1], 'bo')
+    # plt.plot(boys_normed[ : ,0], boys_normed[ : ,1], 'ro')
+    # plt.plot(girls_normed[ : ,0], girls_normed[ : ,1], 'bo')
+    # plt.plot([0, perceptron[0]/-perceptron[2]],[perceptron[0]/-perceptron[1], 0])
+    plt.show()     
+    
+    
+    
+    
+#  从csv文件获取所有数据
+trainInput, trainOutput, testInput, testOutput, boys, girls = loadData()
 
-# plt.plot(boys[ : ,0], boys[ : ,1], 'ro')
-# plt.plot(girls[ : ,0], girls[ : ,1], 'bo')
-# plt.show()   
+#  通过pocket algorithm求出感知器
+perceptron = pocketAlgorithm(trainInput, trainOutput)
+#print(perceptron)
 
- 
-# 画出规范化之后的数据点分布图
-# plt.plot(testData_normed[ : ,1], testData_normed[ : ,2], 'ro')
-# plt.show()   
+#  测试当前得到感知器在测试数据集上的效果
+testPerceptron(perceptron, testInput, testOutput)
 
-
-# 通过pocket algorithm求出感知器
-testData, testAnswer = loadData()
-testData = np.array(testData)
-print(testData)
-print(testAnswer)
-
-testData_normed = testData / testData.max(axis=0)
-print(testData_normed)
-
-perceptron = pocketAlgorithm(testData_normed, testAnswer)
-print(perceptron)
-
-
-# 画出数据点与得到的感知器
-# plt.plot(testData_normed[ : ,1], testData_normed[ : ,2], 'ro')
-# plt.plot([0, perceptron[0]/-perceptron[2]],[perceptron[0]/-perceptron[1], 0])
-# plt.show()  
-
-
-# 画出标记真实类别的数据点和得到的感知器
-boys, girls = loadDataSeperate()
-boys = np.array(boys)
-girls = np.array(girls)
-boys_normed = boys / boys.max(axis=0)
-girls_normed = girls / girls.max(axis=0)
-plt.plot(boys_normed[ : ,0], boys_normed[ : ,1], 'ro')
-plt.plot(girls_normed[ : ,0], girls_normed[ : ,1], 'bo')
-plt.plot([0, perceptron[0]/-perceptron[2]],[perceptron[0]/-perceptron[1], 0])
-plt.show() 
-
-
+# 画出标记真实类别的数据点
+drawGraph(perceptron, boys, girls)
